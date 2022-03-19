0bdbf00af288151cbc64a6cd13b6f78af652edab
“Fidelion” <david.d.95@hotmail.com>
2022-03-20 03:13:27 +0800

    SafeMath lib finished

--- backend/src/contracts/libraries/SafeMath.sol
+++ backend/src/contracts/libraries/SafeMath.sol
@@ -14,4 +14,25 @@ library SafeMath {
         require(y <= x, "SafeMath: Subtraction Overflow");
         return r;
     }
+
+    function multiply(uint x, uint y) internal pure returns(uint256) {
+        //gas optimization
+        if(x == 0) {
+            return 0;
+        }
+        uint r = x * y;
+        require(r / x == y, "SafeMath: Multiplication Overflow");
+        return r;
+    }
+
+    function divide(uint x, uint y) internal pure returns(uint256) {
+        require(y > 0, "SafeMath: Multiplication Division");
+        uint r = x / y;
+        return r;
+    }
+
+    function modulo(uint x, uint y) internal pure returns(uint256) {
+        require(y != 0, "SafeMath: Multiplication Modulo");       
+        return x % y;
+    }
 }
